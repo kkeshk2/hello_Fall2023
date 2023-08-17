d95c02fc586c7f50f9f2f6037d264d06b624e69a
alikeshk <akeshk2@huskers.unl.edu>
2023-08-17 17:09:44 -0500

    added partner name and major

--- hello.c
+++ hello.c
@@ -6,5 +6,7 @@ int main(int argc, char **argv) {
     printf("Kareem Keshk\n");
     printf("computer science\n");
     printf("new");
+    printf("Ali Keshk!\n");
+    printf("Computer Science\n");
 
 }
